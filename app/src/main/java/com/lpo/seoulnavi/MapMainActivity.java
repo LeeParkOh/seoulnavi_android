3690b18603e98d1932878639f16d04dedf2dd72e
parkjongkook <inmosty@gmail.com>
2017-08-08 23:12:50 +0900

    레트로핏 추가

--- app/src/main/java/com/lpo/seoulnavi/MapMainActivity.java
+++ app/src/main/java/com/lpo/seoulnavi/MapMainActivity.java
@@ -51,6 +51,7 @@ public class MapMainActivity extends AppCompatActivity implements OnMapReadyCall
 
         // GPS 확인
         mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        /*
         if(!mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
             //GPS 설정화면으로 이동
             Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
@@ -58,6 +59,7 @@ public class MapMainActivity extends AppCompatActivity implements OnMapReadyCall
             startActivity(intent);
             finish();
         }
+        */
     }
 
     /**
@@ -71,18 +73,27 @@ public class MapMainActivity extends AppCompatActivity implements OnMapReadyCall
 
         // 서울 위도, 경도
         LatLng SEOUL = new LatLng(37.56, 126.97);
+                                //(LATITUDE, LONGITUDE)
+        LatLng PARK70 = new LatLng(37.648420, 127.040929); //초안산근린공원
 
         mGMap = map;
         mGMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         mGMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
         mGMap.animateCamera(CameraUpdateFactory.zoomTo(10));
-
         // 마커에 표시될 정보
         MarkerOptions markerOptions = new MarkerOptions();
         markerOptions.position(SEOUL);
         markerOptions.title("서울");
         markerOptions.snippet("한국의 수도");
+
+        MarkerOptions markerOptions2 = new MarkerOptions();
+        markerOptions2.position(PARK70);
+        markerOptions2.title("초안산근린공원");
+        markerOptions2.snippet("한국의 초안산근린공원");
+
         mGMap.addMarker(markerOptions);
+        mGMap.addMarker(markerOptions2);
+
         Log.d(TAG,"왔나 >>>>>>>>>>>>>>>>>>>>>>");
         testSync();
         Log.d(TAG,"왔나 >>>>>>>>>>>>>>>>>>>>>>2");
@@ -92,7 +103,6 @@ public class MapMainActivity extends AppCompatActivity implements OnMapReadyCall
     void testSync() {
         Log.d(TAG, "sync 맞냐??<<<>>>>>");
         mParkInfoRes = SearchParkInfo.searchParkInfo();
-        //Log.d(TAG,">>>>.>"+mParkInfoRes.searchParkInfo.row.get(1).pPark);
         Log.d(TAG, "sync 맞냐??<<<>>>>>end");
     }
 }
